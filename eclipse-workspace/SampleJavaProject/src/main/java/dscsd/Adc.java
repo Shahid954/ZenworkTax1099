4da1e586e49e0d07e17d9e004810d45f6d9113d7
Shahid <mohd.shahid@thinkitive.com>
2023-05-10 16:40:30 +0300

    sample Project Of date andtime

---
+++ eclipse-workspace/SampleJavaProject/src/main/java/dscsd/Adc.java
@@ -0,0 +1,19 @@
+package dscsd;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Adc {
+
+	public static void main(String[] args) {
+		
+		
+		String str="Saturday, May 06, 2023";
+		 String ab = str.replaceAll(str, "28");
+		System.out.println(ab);
+//		WebDriver driver=new ChromeDriver();
+//		driver.manage().window().maximize();
+//		driver.get("https://www.google.com");
+	}
+
+}
